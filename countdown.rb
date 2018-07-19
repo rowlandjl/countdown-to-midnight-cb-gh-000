721f2bd70d17064ad73913bca84e51b0f594925a
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-19 00:43:23 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n > 0 do
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  sleep(n)
+  while n > 0 do
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
